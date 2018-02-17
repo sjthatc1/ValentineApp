9437544de2a831139b01c7ffd33e91f36e6bbb95
sjthatc1 <thatcherstephen2@gmail.com>
2018-02-17 12:09:30 -0500

    settings buttons to actions not outputs
    added screenshot
    
    changing buttons to actions not outputs

--- Valentine/ViewController.swift
+++ Valentine/ViewController.swift
@@ -31,6 +31,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var profileImg: UIImageView!
     
+    @IBAction func batSignal(_ sender: Any) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
